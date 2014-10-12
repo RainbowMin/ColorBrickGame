74009870ae054fa3e4803bd34c1069157dfc5648
RainbowMin <ecust.mj@gmail.com>
2014-10-12 16:16:04 +0800

    add NGUI

---
+++ Assets/Sripts/ExitButton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour 
+{
+
+	private GameObject _UI_GameScene;
+	public TweenPosition _Title_TweenPos;
+	public TweenPosition  _GameScene_TweenPos;
+	private bool IsPressExit = false;
+	
+	void Awake()
+	{
+		//_UI_GameScene = GameObject.FindGameObjectWithTag ("UI_GameScene");
+		_Title_TweenPos = GameObject.FindGameObjectWithTag ("UI_TitleMenu").GetComponent<TweenPosition>();
+		_GameScene_TweenPos = GameObject.FindGameObjectWithTag ("UI_GameScene").GetComponent<TweenPosition>();
+		
+		_Title_TweenPos.enabled = false;
+		_GameScene_TweenPos.enabled = false;
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//UI_GameScene.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void PressExit()
+	{
+		IsPressExit = true;
+		_Title_TweenPos.gameObject.SetActive (true);
+		_Title_TweenPos.PlayReverse ();
+		_GameScene_TweenPos.PlayReverse ();
+	}
+
+	private void FinishMoveAnimation()
+	{
+		if(IsPressExit)
+		{
+			_GameScene_TweenPos.gameObject.SetActive (false);
+			IsPressExit = false;
+		}
+	}
+}
---
+++ Assets/Sripts/ScreenHelper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenHelper : MonoBehaviour 
+{
+	public static ScreenHelper _instance;
+	public  int height, width;
+
+	void Awake()
+	{
+		_instance = this;
+		_instance.SetScreenWidthAndHeight ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void  SetScreenWidthAndHeight()
+	{
+		UIRoot root = GameObject.FindObjectOfType<UIRoot>();
+		if (root != null) 
+		{
+			float s = (float)root.activeHeight / Screen.height;
+			height =  Mathf.CeilToInt(Screen.height * s);
+			width = Mathf.CeilToInt(Screen.width * s);
+//			Debug.Log("height = " + height);
+//			Debug.Log("width = " + width);
+		}
+	}
+}
